09527e9b0227a33aa66dd82d270fc045bb89b00e
Scott Hyndman <scott@madewithfelt.com>
2021-04-09 17:16:03 -0400

    Add image fade-in while the user is scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,43 @@
-console.log('hi');
+(() => {
+  const hasIntersectionObserver = 'IntersectionObserver' in window;
+  let intersectionObserver;
+  if (hasIntersectionObserver) {
+    intersectionObserver = new IntersectionObserver(
+      (entries, _) => {
+        for (const entry of entries) {
+          if (!entry.isIntersecting) {
+            continue;
+          }
+
+          const img = entry.target;
+          if (!img.classList.contains('is-in-view')) {
+            img.classList.add('is-in-view');
+          }
+        }
+      },
+      {
+        threshold: 0.3,
+      },
+    );
+  }
+
+  for (const img of Array.from(document.querySelectorAll('img'))) {
+    if (!hasIntersectionObserver) {
+      img.classList.add('is-in-view');
+    } else {
+      intersectionObserver.observe(img);
+    }
+
+    if (img.complete) {
+      markImageLoaded(img);
+    } else {
+      img.addEventListener('load', (event) => {
+        markImageLoaded(event.target);
+      });
+    }
+  }
+
+  function markImageLoaded(img) {
+    img.classList.add('is-loaded');
+  }
+})();
